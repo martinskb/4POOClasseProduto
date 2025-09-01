56b8d7bff277d9c7e0902fe74bbb0c7cc78e1eee
kaio <kaio22barra@gmail.com>
2025-09-01 17:55:27 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+4. Classe Produto (Setters com Validação)
+• Crie a classe Produto com atributos privados:
+◦ nome (String)
+◦ preco (double)
+• Crie getters e setters, mas o setPreco() deve validar:
+◦ não permitir preços negativos.
+• No main, teste criando produtos e tentando atribuir valores
+inválidos.
+*/
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Produto p1 = new Produto("Notebook", 2500.00);
+        Produto p2 = new Produto("Mouse", 50.00);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(p1.getNome() + ": R$" + p1.getPreco());
+        System.out.println(p2.getNome() + ": R$" + p2.getPreco());
+
+
+        p1.setPreco(-100);
+        p2.setPreco(75.50);
+
+        System.out.println("Preços atualizados:");
+        System.out.println(p1.getNome() + ": R$" + p1.getPreco());
+        System.out.println(p2.getNome() + ": R$" + p2.getPreco());
     }
 }
