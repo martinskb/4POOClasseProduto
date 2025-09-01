9391e91d35d9fda9c97d0ddbf6d7ef9f40929c2e
kaio <kaio22barra@gmail.com>
2025-09-01 17:55:55 -0300

    Update Produto.java

--- src/Produto.java
+++ src/Produto.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class Produto {
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        setPreco(preco);
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        if (preco < 0) {
+            System.out.println("Erro: Preço não pode ser negativo!");
+        } else {
+            this.preco = preco;
+        }
+    }
 }
